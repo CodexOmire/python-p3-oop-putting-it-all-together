66606ba3b800a3b30b6b0e586e5d51694e5a2d95
CodexOmire <ezekielomire@gmail.com>
2025-08-26 08:32:09 +0300

    Updated book.py

--- lib/book.py
+++ lib/book.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 class Book:
-     def __init__(self,title = "And then they were none",page_count = 272):
+     def __init__(self,title ,page_count):
         self.title = title
         self.page_count = page_count
 
@@ -10,11 +10,11 @@ class Book:
         return self._page_count
 
     @page_count.setter
-    def page_count(self, value):
-        if not isinstance(value, int):
+    def page_count(self, page_count):
+        if not isinstance(page_count, int):
             print("page_count must be an integer")
         else:
-            self._page_count = value
+            self._page_count = page_count
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
